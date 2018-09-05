316ca7f9fc87da89ee877d2a96d2e2f32fd65032
Kenny <ksperkins.ksp@gmail.com>
2018-09-05 11:10:28 -0700

    embeded video and uses ajax call return

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,9 +1,4 @@
-// $(document).ready(function(){
-//     $('.sidenav').sidenav(); /* Mobile Navbar function */
-//     $(".dropdown-trigger").dropdown(); /* Navbar Dropdown Function */
-// });
-
-// var queryURL = "https://liquipedia.net/leagueoflegends/api.php?action=query&prop=revisions&rvprop=content&format=jsonfm&formatversion=2&titles=Main%20Page"
+var vidIdNum;
 
 $.ajax({
     cache: false,
@@ -11,23 +6,29 @@ $.ajax({
         key: 'AIzaSyB-v9METQVhHGLIMj_NF_lNJBizORFDG1s',
         q: 'overwatch world cup 2017 final',
         part: 'snippet'
-    }, {maxResults:20,pageToken:$("#pageToken").val()}),
+    }, { maxResults: 20, pageToken: $("#pageToken").val() }),
     dataType: 'json',
     type: 'GET',
     timeout: 5000,
     url: 'https://www.googleapis.com/youtube/v3/search'
 })
-.done(function(data) {
-    console.log(data);
-    console.log(data.items[0].id.videoId);
-});
+    .done(function (data) {
+        console.log(data);
+        console.log(data.items[0].id.videoId);
+        vidIdNum = data.items[0].id.videoId;
 
-// $.ajax({
-//     url: 'https://liquipedia.net/overwatch/api.php?format=jsonty&action=query&meta=siteinfo',
-//     data: {
-//         format: 'json'
-//     },
-//     dataType: 'jsonp'
-// }).done( function ( data ) {
-//     console.log(data);
-// } );
+        // Load the IFrame Player API code asynchronously.
+        var tag = document.createElement('script');
+        tag.src = "https://www.youtube.com/player_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    });
+
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+        height: '360',
+        width: '640',
+        videoId: vidIdNum
+    });
+}
